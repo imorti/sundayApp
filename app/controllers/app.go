687546eb43a9186e1a12012aef14ae4691e00bde
imorti <imorti@gmail.com>
2016-08-07 13:05:47 -0400

    now with validation on our Sunday app.

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -2,7 +2,6 @@ package controllers
 
 import "github.com/revel/revel"
 
-
 type App struct {
 	*revel.Controller
 }
@@ -10,4 +9,17 @@ type App struct {
 func (c App) Index() revel.Result {
 	greeting := "Aloha from Sunday App"
 	return c.Render(greeting)
+}
+
+func (c App) Hello(myName string) revel.Result {
+	c.Validation.Required(myName).Message("sorry but your name is required.")
+	c.Validation.MinSize(myName, 3).Message("c'mon your name is longer than at least 2 letters...")
+
+	if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(App.Index)
+	}
+
+	return c.Render(myName)
 }
